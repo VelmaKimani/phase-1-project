8d685e29e882856ff3167f1be1d1b84409d2d837
Velma Kimani <stanvelma16@gmail.com>
2023-10-11 18:09:32 +0300

    Add comments

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ document.addEventListener('DOMContentLoaded', () =>{
 const input = document.querySelector("#input");
 const city = document.querySelector("#city");
 const cityName = document.querySelector("#cityName");
-const temp = document.querySelector("#temp");  //
+const temp = document.querySelector("#temp");  // select the necessary ids whose values will be updated
 const main = document.querySelector("#main");
 const description = document.querySelector("#description");
 const image = document.querySelector("#image");
 
 input.onsubmit = (e) => {
-  e.preventDefault();
+  e.preventDefault();  //Ensure the form does not submit prematurely
   weatherUpdate(city.value);
   city.value = "";
 };
 
 weatherUpdate = (city) => {
 fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=78fbb2284a9b5c083789b0273c025e2b`)
-.then((res) => res.json())
+.then((res) => res.json()) //fetch data from the API
 .then((data) => {
     console.log(data)
     let status;
